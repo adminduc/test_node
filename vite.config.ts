0d6b0bcf2eaf6262cbf552e1ce7a530efc35ad9b
MinhDuc <ogdp.vn@gmail.com>
2023-07-05 10:30:32 +0700

    dsfds

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import dotenv from 'dotenv';
-import { VitePluginNode } from 'vite-plugin-node';
+import dotenv from "dotenv";
+import { VitePluginNode } from "vite-plugin-node";
 dotenv.config();
 
 const config = () => {
-    return {
-        server: {
-            port: process.env.PORT,
-        },
-        plugins: [
-            ...VitePluginNode({
-                adapter: 'express',
-                appPath: './src/app.ts',
-                exportName: 'viteNodeApp',
-                tsCompiler: 'esbuild',
-                swcOptions: {},
-            }),
-        ],
-        optimizeDeps: {},
-    };
+  return {
+    server: {
+      port: Number(process.env.PORT) | 3000,
+    },
+    plugins: [
+      ...VitePluginNode({
+        adapter: "express",
+        appPath: "./src/app.ts",
+        exportName: "viteNodeApp",
+        tsCompiler: "esbuild",
+        swcOptions: {},
+      }),
+    ],
+    optimizeDeps: {},
+  };
 };
 
 export default config;
