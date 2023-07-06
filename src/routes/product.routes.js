6b106bff2bcc9174f29bbe2582148fb03f21722c
MinhDuc <ogdp.vn@gmail.com>
2023-07-07 06:23:40 +0700

    first node

--- src/routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-let router = require("express").Router();
-const ProductController = require("../controllers/product.controller");
-const auth = require("../middleware/auth.middleware");
-//public
-router.get("/products", ProductController.get);
-
-module.exports = router;
